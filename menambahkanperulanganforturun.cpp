08a22868869ffc8e167320d34066120c26fa0a27
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-21 10:31:02 +0700

    menambahkanperulanganforturun.cpp

--- menambahkanperulanganforturun.cpp
+++ menambahkanperulanganforturun.cpp
@@ -16,3 +16,13 @@ for (int i = 0; i < 5; i++)
 }
 
 cout << endl;
+
+// Perulangan dengan FOR untuk turun
+cout << "PERULANGAN FOR TURUN" << endl;
+
+for (int i = 5; i > 0; i--)
+{
+    cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+}
+
+cout << endl;
