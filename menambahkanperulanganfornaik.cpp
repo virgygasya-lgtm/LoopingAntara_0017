b22ae6d00fb901c1cfb9891729a551d26bdbf993
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-21 10:28:54 +0700

    menambahkanperulanganfornaik.cpp

---
+++ menambahkanperulanganfornaik.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+} 
+
+// Perulangan dengan FOR
+cout << "PERULANGAN FOR" << endl;
+
+for (int i = 0; i < 5; i++)
+{
+    cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+}
+
+cout << endl;
