d7eead21a2c60cb5324718629bc7e5badb4c4aac
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-21 10:33:21 +0700

    Loopingfor.cpp

---
+++ Loopingfor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Perulangan dengan FOR
+    cout << "PERULANGAN FOR" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+    }
+
+    cout << endl;
+
+    // Perulangan dengan FOR untuk turun
+    cout << "PERULANGAN FOR TURUN" << endl;
+
+    for (int i = 5; i > 0; i--)
+    {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
