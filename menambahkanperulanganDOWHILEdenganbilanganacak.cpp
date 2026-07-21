4c6ba67a30ca63b939d1a1101aa2d0e2877259fb
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-21 10:55:36 +0700

    menambahkanperulanganDOWHILEdenganbilanganacak.cpp

--- menambahkanperulanganDOWHILEdenganbilanganacak.cpp
+++ menambahkanperulanganDOWHILEdenganbilanganacak.cpp
@@ -13,3 +13,12 @@ srand(time(0));
 
 // Nilai awal x = 1
 x = 1 + rand() % 10;
+
+cout << "PERULANGAN DO...WHILE" << endl;
+
+do
+{
+    cout << "TI, Bilangan acak = " << x << endl;
+    x = rand() % 10;
+}
+while (x <= 5);
