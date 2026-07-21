0d7283d160929dc4dbb457e35190f68dff721d05
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-21 10:45:16 +0700

    menambahkanperulanganWHILEdenganbilanganacak.cpp

--- menambahkanperulanganWHILEdenganbilanganacak.cpp
+++ menambahkanperulanganWHILEdenganbilanganacak.cpp
@@ -14,3 +14,11 @@ srand(time(0));
 
 // Nilai awal x = 1
 x = 1 + rand() % 10;
+
+cout << "PERULANGAN WHILE" << endl;
+
+while (x <= 5)
+{
+    cout << "TI, Bilangan acak = " << x << endl;
+    x = rand() % 10;
+}
